c072a005ee7dff5b65976c3502f07a621a096755
petekgithub <petekshamitbeyli@gmail.com>
2022-09-14 02:45:28 +0300

    intro-style-v1-notfinished

--- src/pages/HomePage/Intro/index.tsx
+++ src/pages/HomePage/Intro/index.tsx
@@ -3,12 +3,10 @@ import styles from "./styles.module.scss";
 import GetStartedButton from 'components/Button/GetStartedButton';
 
 
-
 //export type Logo = "svg" | "png";
 // export interface ILogo {
 //   src: string | StatticImageData;
 // }
-
 
 
 const Intro = () => {
@@ -16,18 +14,17 @@ const Intro = () => {
     <div>
       <section id="Intro" className={styles.container}>
         <div className={styles.content}>
-          <div className={styles.explanation}>
-            <h1>Make The Best Financial Decisions</h1>
-            <h4>Cum Et Convallis Risus Placerat Aliquam, Nuno, Scelerisque Aliquet Faucibus Tincidunt Eu Adipiscing Sociis Arcu Lorem Porttitor.</h4>
+          <div className={styles.info}>
+            <h1 className={styles.title}>Make The Best Financial Decisions</h1>
+            <span>Cum Et Convallis Risus Placerat Aliquam, Nuno, Scelerisque Aliquet Faucibus Tincidunt Eu Adipiscing Sociis Arcu Lorem Porttitor.</span>
+            <img className={styles.light} src="/assets/introlight.svg" alt="IntroLight"/>  
           </div>
           <div className={styles.media}> 
             <GetStartedButton />
             <img src="/assets/introwatch.svg" alt="IntroWatch"/>
-            
           </div>
-          {/* <img src={'string' as any} alt="IntroPhone"/> */}
-          <img src="/assets/intrologo.svg" alt="IntroLogo"/>
-          
+          <img className={styles.phone} src="/assets/introphone.png"alt="IntroPhone"/> 
+          <img src="/assets/intrologo.svg" alt="IntroLogo"/>   
         </div>
       </section>
     </div>
