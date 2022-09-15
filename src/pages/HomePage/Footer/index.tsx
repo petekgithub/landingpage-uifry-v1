7e7564123b0b1476547db9c6ef63df5aa9f3065c
petekgithub <petekshamitbeyli@gmail.com>
2022-09-16 02:31:02 +0300

    footer-style-done

--- src/pages/HomePage/Footer/index.tsx
+++ src/pages/HomePage/Footer/index.tsx
@@ -5,47 +5,58 @@ import styles from "./styles.module.scss";
 const Footer = () =>  {
   return (
     <section id="Footer" className={styles.container}>
-       <div className={styles.newsletter}>
-          <h1>Newsletter</h1>
-          <span>Stay Up To Date</span>
-          <input type="email" name="email" className="subscribe-input" placeholder="Your email"/>
-          <div className={styles.submitButton}>Subscribe</div>
-       </div> 
-       <div className={styles.contact}>
-          <img src="/assets/headerlogo.svg" />
-          {/* mail icon */}
-          <span>Help@Frybix.com</span>
-          {/* phone icon  */}
-          <span>+1234 456 678 89</span>
+        <div className={styles.innerContainer}>
+            <div className={styles.contact}>
+                <img src="/assets/headerlogo.svg" />
+                <div className={styles.mail}>
+                  <img src="/assets/mail.svg" />
+                  <span>Help@Frybix.com</span>
+                </div>
+                <div className={styles.phone}>
+                  <img src="/assets/phone.svg" />
+                  <span>+1234 456 678 89</span>
+                </div>
+            </div> 
+
+              <div className={styles.links}>
+                  <ul>
+                    <h3>Links</h3>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Bookings</li>
+                    <li>Blogs</li>
+                  </ul>
+              </div>
+              <div className={styles.legal}>
+                  <ul>
+                    <h3>Legal</h3>
+                    <li>Terms Of Use</li>
+                    <li>Privacy Policy</li>
+                    <li>Cookie Policy</li>
+                  </ul>
+              </div>
+              <div className={styles.product}>
+                  <ul>
+                    <h1>Product</h1>
+                    <li>Take Tour</li>
+                    <li>Live Chat</li>
+                    <li>Reviews</li>
+                  </ul>
+              </div>
+            
+
+            <div className={styles.newsletter}>
+              <h1>Newsletter</h1>
+              <h4>Stay Up To Date</h4>
+              <input type="email" name="email" className={styles.subscribeInput} placeholder="Your email"/>
+              <button type="submit" className={styles.submitButton}>Subscribe</button>
+            </div> 
        </div>
-       <div className={styles.infos}>
-        <div className={styles.links}>
-            <ul>
-              <h3>Links</h3>
-              <li>Home</li>
-              <li>About Us</li>
-              <li>Bookings</li>
-              <li>Blogs</li>
-            </ul>
-        </div>
-        <div className={styles.legal}>
-            <ul>
-              <h3>Terms Of Use</h3>
-              <li>Privacy Policy</li>
-              <li>Cookie Policy</li>
-            </ul>
-        </div>
-        <div className={styles.product}>
-            <ul>
-              <h3>Take Tour</h3>
-              <li>Live Chat</li>
-              <li>Reviews</li>
-            </ul>
-        </div>
-       </div>
+
        <div className={styles.frame}>
           <p>Copyright 2022 Uifry.Com All Rights Reserved </p>
        </div>
+
     </section>
   )
 }
