fa8f8cb4e0adc555875c9d9e670717cdbd999de0
petekgithub <petekshamitbeyli@gmail.com>
2022-09-27 00:46:10 +0300

    eslint-update-tells-forgotten-log

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,9 @@ import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
+    // <Component{...pageProps} />
     <>
       <Head>
         <title>Uifry - Landing Page</title>
